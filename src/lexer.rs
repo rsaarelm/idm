4ff2d993cf772dc4ba0af8edff68c178dc8f1c8c
Risto Saarelma <risto.saarelma@iki.fi>
2021-08-11 13:43:27 +0300

    Lexer module

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,5 @@
 pub type Result<'a, T> = std::result::Result<T, &'a str>;
+use std::fmt;
 
 /// Low-level indented text parsing primitives.
 ///
@@ -8,24 +9,52 @@ pub type Result<'a, T> = std::result::Result<T, &'a str>;
 /// failure will fail the entire parse.
 #[derive(Clone, Eq, PartialEq)]
 pub struct Lexer<'a> {
-    input: &'a str,
+    /// Is the indentation based on spaces or tabs.
+    ///
+    /// Will be `None` at the start of the input. The first indentation
+    /// encountered will set `indent_char`.
     indent_char: Option<char>,
+
+    /// The current indentation prefix, as a list of segment lenghts.
+    ///
+    /// Segments correspond to previous indentation levels. Dedenting is only
+    /// allowed to to segment boundaries. A dedent that does not line up with
+    /// the established indent segments is a syntax error.
+    ///
+    /// An empty vector corresponds to the special logical indent level -1.
+    /// Regular level 0 indent is represented by having a single 0-valued
+    /// element at the start of the list.
     indent_segments: Vec<usize>,
+
+    /// The remaining input to be lexed.
+    input: &'a str,
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(input: &str) -> Result<Lexer> {
-        // TODO: Fail if first line does not have zero indent
+        // First line must have indentation depth 0.
+        match parse::indent(input)? {
+            (n, rest) if !n.is_empty() => return Err(rest),
+            _ => {}
+        }
+
         Ok(Lexer {
-            input,
             indent_char: Default::default(),
             indent_segments: Default::default(),
+            input,
         })
     }
 
     /// Succeed if only whitespace remains of input.
     pub fn end(&mut self) -> Result<()> {
-        todo!();
+        for (i, c) in self.input.char_indices() {
+            if !c.is_whitespace() {
+                return Err(&self.input[i..]);
+            }
+        }
+        self.input = "";
+        self.indent_segments.clear();
+        Ok(())
     }
 
     /// Enter body of current headline.
@@ -40,28 +69,129 @@ impl<'a> Lexer<'a> {
     ///
     /// Will fail when there is neither a headline (at depth -1 or indented to
     /// an empty body) nor any body lines.
-    pub fn start_body(&mut self) -> Result<Option<&str>> {
-        todo!();
+    pub fn enter_body(&mut self) -> Result<Option<&'a str>> {
+        // At EOF.
+        // - Can't have headline or body, fail out.
+        if self.input == "" {
+            return Err(self.input);
+        }
+
+        let (current_prefix, _) = parse::indent(self.input)?;
+        let new_segments: Vec<usize> =
+            self.match_indent(current_prefix).map_err(|_| self.input)?;
+
+        // Current indent is shorter than expected indent.
+        // - Can't have headline or body, fail out
+        if new_segments.len() < self.indent_segments.len() {
+            return Err(self.input);
+        }
+
+        self.witness_indentation_char(current_prefix)
+            .map_err(|_| self.input)?;
+
+        // A headline exists if content starts *exactly* at indent string.
+        let (line, rest) = parse::line(self.input)?;
+
+        // Current indent is longer than expected indent.
+        // - Headline is None.
+        // - Do not move input, first line of input already belongs to body.
+        if new_segments.len() > self.indent_segments.len() {
+            self.indent_segments = new_segments;
+            return Ok(None);
+        }
+
+        debug_assert!(new_segments.len() == self.indent_segments.len());
+        debug_assert!(new_segments.len() > 0);
+
+        // Headline indent as expected, but headline is blank.
+        //  - Move to next line
+        //  - Cannot have children (because of how indent for blank lines is
+        //    determined), set synthetic +1 indent level.
+        //  - Return Some("") for headline
+        if line.chars().all(|c| c.is_whitespace()) {
+            self.input = rest;
+            self.indent_segments.push(1);
+            return Ok(Some(""));
+        }
+
+        // Headline indent as expected, headline has content
+        //  - Move to next line
+        self.input = rest;
+        //  - Determine indent level for next line
+        let (body_prefix, _) = parse::indent(self.input)?;
+        let body_segments =
+            self.match_indent(body_prefix).map_err(|_| self.input)?;
+        //  - If next line's indent level is larger than expected, set
+        //    expected to that
+        //  - Otherwise set expected indent level to synthetic +1 indent
+        if body_segments.len() > self.indent_segments.len() {
+            self.indent_segments = body_segments;
+        } else {
+            self.indent_segments.push(1);
+        }
+
+        //  - Parse headline content, minus indentation into result
+        Ok(Some(&line[current_prefix.len()..]))
     }
 
     /// Pop out of current indented body even if there is more body content
     /// left.
     ///
     /// Used to parse content for the special `_contents` struct field.
-    pub fn dedent(&mut self) -> Result<()> {
-        todo!();
+    pub fn dedent(&mut self) {
+        self.indent_segments
+            .pop()
+            .expect("Lexer::dedent already at bottom of indent stack");
     }
 
     /// Pop out of current indented body when there is only white space left
     /// in the body. Will fail if body still has content.
-    pub fn end_body(&mut self) -> Result<()> {
-        todo!();
+    pub fn exit_body(&mut self) -> Result<()> {
+        let (body_prefix, _) = parse::indent(self.input)?;
+        let body_segments =
+            self.match_indent(body_prefix).map_err(|_| self.input)?;
+        if body_segments.len() < self.indent_segments.len() {
+            self.dedent();
+            Ok(())
+        } else {
+            Err(self.input)
+        }
     }
 
     /// Extract a single word from the current line, move lexer to start of
     /// next word on the same line or end of line, whichever is closest.
     pub fn word(&mut self) -> Result<&str> {
-        todo!();
+        if self.input == "" {
+            return Err(self.input);
+        }
+
+        let (line, _) = parse::line(self.input)?;
+        if line.chars().all(|c| c.is_whitespace()) {
+            return Err(self.input);
+        }
+
+        let mut start_pos = 0;
+        let mut end_pos = line.len();
+        let mut before_word = true;
+        for (i, c) in line.char_indices() {
+            if before_word {
+                if c.is_whitespace() {
+                    start_pos = i;
+                } else {
+                    before_word = false;
+                }
+            } else {
+                if c.is_whitespace() {
+                    end_pos = i;
+                    break;
+                }
+            }
+        }
+
+        let word = &self.input[start_pos..end_pos];
+        self.input = &self.input[end_pos..];
+
+        Ok(word)
     }
 
     /// Read section at lexer into string with the indentation up to headline
@@ -70,7 +200,246 @@ impl<'a> Lexer<'a> {
     /// If run when there is no headline, only the body is read and
     /// indentation is removed up to body level.
     pub fn section(&mut self) -> Result<String> {
-        todo!();
+        let mut ret = String::new();
+        self.read_section(&mut ret)?;
+        if !ret.is_empty() {
+            // Drop trailing newline.
+            ret.pop();
+        }
+        Ok(ret)
+    }
+}
+
+// Private methods
+
+impl<'a> Lexer<'a> {
+    /// Read a section into an existing string buffer.
+    fn read_section(&mut self, buffer: &mut String) -> Result<()> {
+        let (current_prefix, _) = parse::indent(self.input)?;
+        self.witness_indentation_char(current_prefix)
+            .map_err(|_| self.input)?;
+        let new_segments: Vec<usize> =
+            self.match_indent(current_prefix).map_err(|_| self.input)?;
+
+        if new_segments.len() < self.indent_segments.len() {
+            // Dropped out of depth, no go.
+            return Err(self.input);
+        }
+
+        let dedent_segments = if new_segments.len() > self.indent_segments.len()
+        {
+            // No headline, read body directly, dedent at body depth.
+            new_segments
+        } else {
+            // Headline is there, dedent at headline depth.
+            self.indent_segments.clone()
+        };
+
+        let indent_len: usize = dedent_segments.iter().sum();
+        while self.input != "" {
+            // Verify indentation at new level.
+            let (line, rest) = parse::line(self.input)?;
+            let (indent, _) = parse::indent(self.input)?;
+            self.witness_indentation_char(indent)
+                .map_err(|_| self.input)?;
+            let line_segments =
+                self.match_indent(indent).map_err(|_| self.input)?;
+
+            if line_segments.len() < dedent_segments.len() {
+                // Out of depth, exit.
+                break;
+            }
+
+            if line.chars().all(|c| c.is_whitespace()) {
+                // Blank line.
+                buffer.push('\n');
+            } else {
+                buffer.push_str(&line[indent_len..]);
+                buffer.push('\n');
+            }
+            self.input = rest;
+        }
+        Ok(())
+    }
+
+    // XXX: Using unit errors because the input &str errors were giving the
+    // borrow checker some trouble over at `enter_body`.
+
+    /// Match an indentation prefix with the current indent segment state.
+    ///
+    /// If the indentation does not match the current input string, either
+    /// by segment joints or indent char, fail.
+    ///
+    /// An indentation at the fake -1 column can only match indentations at
+    /// column 0.
+    fn match_indent(
+        &self,
+        prefix: &str,
+    ) -> std::result::Result<Vec<usize>, ()> {
+        // Empty segment is always good.
+        if prefix == "" {
+            return Ok(vec![0]);
+        }
+
+        // At column -1 it's an error to start with any indent other than 0.
+        if self.indent_segments.is_empty() && !prefix.is_empty() {
+            return Err(());
+        }
+
+        // Must be good to unwrap since we've already handled the empty string
+        // case.
+        let indent_char = prefix.chars().next().unwrap();
+        debug_assert!(prefix.chars().all(|c| c == indent_char));
+        if indent_char != self.indent_char.unwrap_or(indent_char) {
+            // Mixed indentation, fail.
+            return Err(());
+        }
+
+        let mut ret = Vec::new();
+        let mut segments_len = 0;
+        for &seg in &self.indent_segments {
+            segments_len += seg;
+            ret.push(seg);
+            if segments_len == prefix.len() {
+                // Exact match to some subsequence, exit now.
+                return Ok(ret);
+            } else if segments_len > prefix.len() {
+                // Prefix does not align with existing segments.
+                return Err(());
+            }
+        }
+
+        // Add the final bit of prefix.
+        debug_assert!(prefix.len() > segments_len);
+        ret.push(prefix.len() - segments_len);
+        Ok(ret)
+    }
+
+    /// Register indentation char from input prefix.
+    ///
+    /// Must match an established indentation char. If indentation char has
+    /// not been set yet and `indent_prefix` is nonempty, the prefix will be
+    /// the char from `indent_prefix` for the rest of the lexer's lifetime.
+    fn witness_indentation_char(
+        &mut self,
+        indent_prefix: &str,
+    ) -> std::result::Result<(), ()> {
+        if let Some(c) = indent_prefix.chars().next() {
+            // If this was a public API we'd maybe need a newtype for
+            // indent_prefix that takes care of these invariants.
+            debug_assert!(c == ' ' || c == '\t');
+            debug_assert!(indent_prefix.chars().all(|ch| ch == c));
+
+            if c != self.indent_char.unwrap_or(c) {
+                // Mismatch!
+                return Err(());
+            }
+
+            self.indent_char = Some(c);
+            Ok(())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl<'a> fmt::Debug for Lexer<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Custom debug formatter that cuts down an over-long input string.
+        const MAX_LEN: usize = 72;
+        write!(
+            f,
+            "Lexer {{\n    indent_char: {:?},\n    indent_segments: {:?},\n    input: ",
+            self.indent_char.unwrap_or('\0'),
+            self.indent_segments
+        )?;
+
+        if self.input.len() > MAX_LEN {
+            let cut_pos = self
+                .input
+                .char_indices()
+                .take(MAX_LEN - 3)
+                .map(|(i, _)| i)
+                .last()
+                .unwrap_or(0);
+            write!(f, "{:?}", &self.input[0..cut_pos])?;
+        } else {
+            write!(f, "{:?}", self.input)?;
+        }
+        writeln!(f, " }}")
+    }
+}
+
+// Parsing primitives that don't rely on Lexer state.
+
+mod parse {
+    type Result<'a, T> = std::result::Result<(T, &'a str), &'a str>;
+
+    /// Indentation for any line.
+    ///
+    /// If the line is blank and `line_indent` would return `None`, use the
+    /// indent of the next line. EOF gets indent `""`.
+    ///
+    /// Moves input to position after the first well-defined indent prefix or
+    /// at EOF, whichever comes first.
+    pub fn indent(input: &str) -> Result<&str> {
+        let mut pos = input;
+
+        loop {
+            match line_indent(pos)? {
+                (Some(prefix), rest) => return Ok((prefix, rest)),
+                (None, rest) => {
+                    debug_assert!(rest.len() < pos.len());
+                    pos = rest;
+                }
+            }
+        }
+    }
+
+    /// Indentation for a single line, if defined.
+    fn line_indent(input: &str) -> Result<Option<&str>> {
+        let mut indent_char = None;
+
+        for (i, c) in input.char_indices() {
+            if !c.is_whitespace() {
+                // Content encountered, return what was read so far.
+                return Ok((Some(&input[..i]), &input[i..]));
+            }
+            if c == '\n' {
+                // Hit end of line with no content, no well-defined indentation.
+                return Ok((None, &input[i + 1..]));
+            }
+            match indent_char {
+                None => {
+                    if c == ' ' || c == '\t' {
+                        indent_char = Some(c);
+                    } else {
+                        return Err(&input[i..]);
+                    }
+                }
+                Some(ic) => {
+                    // Mixed indentation detected.
+                    if ic != c {
+                        return Err(&input[i..]);
+                    }
+                }
+            }
+        }
+
+        // At the end of input, by convention EOF has indent level 0.
+        Ok((Some(""), ""))
+    }
+
+    /// Read line until newline, rest of the content is after newline.
+    ///
+    /// Fails if there is no input.
+    pub fn line(input: &str) -> Result<&str> {
+        if input == "" {
+            Err(input)
+        } else {
+            let p = input.find('\n').unwrap_or(input.len());
+            Ok((&input[..p], &input[(p + 1).min(input.len())..]))
+        }
     }
 }
 
@@ -78,8 +447,38 @@ impl<'a> Lexer<'a> {
 mod tests {
     use super::*;
 
+    fn t(input: &str) -> Lexer {
+        Lexer::new(input).unwrap()
+    }
+
     #[test]
-    fn test_lexer() {
-        assert!(false);
+    fn lexer_end() {
+        assert!(t("a").end().is_err());
+        assert!(t("xyzzy  ").end().is_err());
+        assert!(t("").end().is_ok());
+        assert!(t("\n\n").end().is_ok());
+    }
+
+    #[test]
+    fn lexer_enter_body() {
+        assert_eq!(t("").enter_body(), Err(""));
+        // Lexing starts at column -1.
+
+        let mut lexer = t("a");
+        // Get out of the null layer.
+        assert_eq!(lexer.enter_body(), Ok(None));
+        // Enter the line.
+        assert_eq!(lexer.enter_body(), Ok(Some("a")));
+    }
+
+    #[test]
+    fn lexer_read() {
+        assert_eq!(t("a").section(), Ok("a".into()));
+
+        assert_eq!(t("a\n  b").section(), Ok("a\n  b".into()));
+
+        let mut lexer = t("a\n  b\nc");
+        lexer.enter_body().unwrap();
+        assert_eq!(lexer.section(), Ok("a\n  b".into()));
     }
 }
--- src/new_parse.rs
+++ src/new_parse.rs
@@ -2,12 +2,13 @@
 
 use std::fmt;
 
-pub type Result<'a, T> = std::result::Result<(T, (IndentPrefix, &'a str)), &'a str>;
+pub type Result<'a, T> =
+    std::result::Result<(T, (IndentPrefix, &'a str)), &'a str>;
 
 /// Read item from mutable slice, update slice if read was successful.
 pub fn r<'a, T>(
     s: &mut (IndentPrefix, &'a str),
-    item: impl Fn((IndentPrefix, &'a str)) -> Result<'a, T>
+    item: impl Fn((IndentPrefix, &'a str)) -> Result<'a, T>,
 ) -> std::result::Result<T, &'a str> {
     item(*s).map(|(ret, rest)| {
         *s = rest;
@@ -61,11 +62,13 @@ fn eof((prefix, input): (IndentPrefix, &str)) -> Result<()> {
 
 /// Succeed if there's no content left on input line.
 ///
-/// Move to next line if whitespace was succesfully consumed.
+/// Move to next line if whitespace was succesfully consumed. Will instantly
+/// parse completely empty lines and can be used to skip them when called at
+/// the start of a line.
 fn eol((prefix, input): (IndentPrefix, &str)) -> Result<()> {
     for (i, c) in input.char_indices() {
         if c == '\n' {
-            return Ok(((), (prefix, &input[i+1..])));
+            return Ok(((), (prefix, &input[i + 1..])));
         }
 
         if !c.is_whitespace() {
@@ -75,29 +78,37 @@ fn eol((prefix, input): (IndentPrefix, &str)) -> Result<()> {
     Ok(((), (prefix, "")))
 }
 
-/// Succeed if input is at end of current block of indentation.
+/// Succeed if input is at end of current indented block, with only trailing
+/// whitespace remaining.
 ///
-/// Move to line starting next block if block was successfully ended.
+/// Moves to start of first line outside of the indented block, adjusting
+/// indent prefix in the return value to match that line.
 pub fn eob((prefix, input): (IndentPrefix, &str)) -> Result<()> {
-    // If at eof, also at eob, exit early.
     // Otherwise determine input's indent, it will be defined when not at eob.
     //
     todo!()
 }
 
-// Use eob(input).is_err() to see you're within a block.
+// XXX: EOB is where the system sorta breaks down. It will jump out multiple
+// levels at once, isn't able to check for inconsistent indentation etcetera.
+// Would be *much* nicer if IndentPrefix got promoted to a serious, segmenty
+// IndentString, so EOB will just pop a segment, can detect mismatched
+// indentation and fail on you and so forth. This means constructed
+// IndentStrings, so does IndentString just sort of get threaded along or
+// what? I mean you could clone it, or you could do
+//
+//     parse::r(&mut cursor, parse::primitive)?;
+//
+// So okay now a mut tuple with no-Copy widget needs to be r-able. I can test
+// this out...
 
-/// Enter the indentation level at input position that must be less than
-/// current prefix.
-///
-/// Does not consume anything, affects the indent prefix in the remaining
-/// input parameter instead.
-pub fn dedent((prefix, input): (IndentPrefix, &str)) -> Result<()> {
-    todo!();
-}
+// Use eob(input).is_err() to see you're within a block.
+// Use eob to get the new parse level outside of a block.
 
 /// Parse section of the outline with headline at current input position (line
 /// and exact indent prefix), return result with the indent prefix stripped.
+///
+/// Fails if content at input does not match indent prefix.
 pub fn section((prefix, input): (IndentPrefix, &str)) -> Result<String> {
     // TODO: Helper function that takes &mut String as parameter. Will also be
     // used to implement `block`.
@@ -107,6 +118,8 @@ pub fn section((prefix, input): (IndentPrefix, &str)) -> Result<String> {
 /// Parse remaining sections in current block (content for each must start at
 /// exactly the indent prefix), return string of them with the indent prefix
 /// stripped.
+///
+/// Fails if content at input does not match indent prefix.
 pub fn block((prefix, input): (IndentPrefix, &str)) -> Result<String> {
     // TODO: Just call section helper repeatedly until at EOB.
     todo!();
@@ -115,8 +128,10 @@ pub fn block((prefix, input): (IndentPrefix, &str)) -> Result<String> {
 /// From start of line with headline at prefix indent, move to start of first
 /// body line and adjust indent prefix to that of body.
 ///
-/// Will fail if there are no contentful body lines indented deeper than the
-/// headline.
+/// If there are no actual body lines, the cursor will move to the next line
+/// with an dummy indent level and the only valid parse action is `eob`.
+///
+/// Fails if content at input does not match indent prefix.
 pub fn enter_body((prefix, input): (IndentPrefix, &str)) -> Result<String> {
     todo!();
 }
@@ -128,10 +143,11 @@ pub fn enter_body((prefix, input): (IndentPrefix, &str)) -> Result<String> {
 /// prefix and be followed by non-newline, non-whitespace content.
 ///
 /// Encountering mixed indentation (tabs when previous indent was spaces or
-/// vice versa) will fail the function. If the current input line is at eob,
-/// returns prefix. If current input is a blank line (eol), will return the
-/// indent prefix of the first contentful line encountered when reading ahead
-/// in the same indent block.
+/// vice versa) will fail the function. If current input is a blank line
+/// (eol), will return the indent prefix of the first contentful line
+/// encountered when reading ahead in the same indent block. If at eof and
+/// there is no more contentful lines, eof will be treated as an unindented
+/// content line and the prefix will be empty.
 ///
 /// It's assumed that `input` will be at the start of a line when this is
 /// called.
@@ -140,7 +156,9 @@ fn indent((prefix, input): (IndentPrefix, &str)) -> Result<()> {
 }
 
 /// Return indent prefix for a non-blank line, `None` otherwise.
-fn line_indent((prefix, input): (IndentPrefix, &str)) -> Result<Option<IndentPrefix>> {
+fn line_indent(
+    (prefix, input): (IndentPrefix, &str),
+) -> Result<Option<IndentPrefix>> {
     if eol((prefix, input)).is_ok() {
         return Ok((None, (prefix, input)));
     }
@@ -165,15 +183,20 @@ fn line_indent((prefix, input): (IndentPrefix, &str)) -> Result<Option<IndentPre
         }
     }
 
-    Ok((Some(IndentPrefix { indent_char: Some(indent_char), len }), (prefix, &input[len..])))
+    Ok((
+        Some(IndentPrefix {
+            indent_char: Some(indent_char),
+            len,
+        }),
+        (prefix, &input[len..]),
+    ))
 }
-
 
 /// Stack-dwelling fake string object.
 ///
 /// Used instead of `&str` for indent prefixes so that dummy indent levels can
 /// be specified even when they don't have a corresponding slice.
-#[derive(Default, Copy, Clone, Eq, PartialEq)]
+#[derive(Default, Copy, Clone, Eq, PartialEq, Debug)]
 pub struct IndentPrefix {
     indent_char: Option<char>,
     len: usize,
@@ -186,7 +209,9 @@ impl fmt::Display for IndentPrefix {
         }
 
         // A dummy indent from unspecified indent level will be unprintable.
-        let ch = self.indent_char.expect("IndentPrefix::fmt: Unprintable dummy indent");
+        let ch = self
+            .indent_char
+            .expect("IndentPrefix::fmt: Unprintable dummy indent");
         for _ in 0..self.len {
             write!(f, "{}", ch)?;
         }
@@ -195,9 +220,10 @@ impl fmt::Display for IndentPrefix {
 }
 
 impl IndentPrefix {
-    fn len(self) -> usize { self.len }
+    fn len(self) -> usize {
+        self.len
+    }
 }
-
 
 #[cfg(test)]
 mod tests {
