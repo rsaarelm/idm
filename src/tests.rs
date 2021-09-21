ceab0b86ce565b656ba863e3b3436e691d42b57e
Risto Saarelma <risto.saarelma@iki.fi>
2021-09-21 11:36:46 +0300

    Comment out the whole flatten test, don't leave an empty test hanging out in the test count

--- src/tests.rs
+++ src/tests.rs
@@ -520,33 +520,33 @@ v:
     );
 }
 
+/*
 #[test]
 fn struct_flatten() {
-    /*
-         XXX: Does not currently work, see https://github.com/serde-rs/serde/issues/1346
-         FIXME if the Serde issue gets resolved.
+    XXX: Does not currently work, see https://github.com/serde-rs/serde/issues/1346
+    FIXME if the Serde issue gets resolved.
 
-         #[derive(Clone, Eq, PartialEq, Default, Debug, Serialize, Deserialize)]
-         struct Nested {
-             #[serde(flatten)]
-             simple: Simple,
-         }
+     #[derive(Clone, Eq, PartialEq, Default, Debug, Serialize, Deserialize)]
+     struct Nested {
+         #[serde(flatten)]
+         simple: Simple,
+     }
 
-         test(
-             "\
-     name-text: Foo bar
-     x: 1
-     y: 2",
-             &Nested {
-                 simple: Simple {
-                     name_text: s("Foo bar"),
-                     x: 1,
-                     y: 2,
-                 },
+     test(
+         "\
+name-text: Foo bar
+x: 1
+y: 2",
+         &Nested {
+             simple: Simple {
+                 name_text: s("Foo bar"),
+                 x: 1,
+                 y: 2,
              },
-         );
-    */
+         },
+     );
 }
+*/
 
 #[test]
 fn struct_contents() {
