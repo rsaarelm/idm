31c2aebfdab062d858c6e71465d945f96e2e6148
Risto Saarelma <risto.saarelma@iki.fi>
2022-04-17 18:27:01 +0300

    Echo example

---
+++ examples/echo.rs
@@ -0,0 +1,22 @@
+use std::io::Read;
+
+use idm;
+use indexmap::IndexMap;
+use serde::{Deserialize, Serialize};
+
+// Deserialize input from stdin into data outline and serialize the outline to
+// stdout. Use this to diagnose any problems with parsing handwritten outlines
+// files into a data outline structure.
+
+#[derive(PartialEq, Default, Debug, Serialize, Deserialize)]
+struct DataOutline((IndexMap<String, String>, Vec<(String, DataOutline)>));
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let outline = idm::from_str::<DataOutline>(&buf).unwrap();
+
+    let reser = idm::to_string_styled_like(&buf, &outline).unwrap();
+    print!("{}", reser);
+}
